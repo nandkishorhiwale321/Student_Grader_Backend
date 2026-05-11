be81fa6fbc04300b8f543c2bf8396137c96482ae
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2026-05-11 15:25:35 +0530

    config2

--- src/main/java/com/StudentGrader/Config/SecurityConfig.java
+++ src/main/java/com/StudentGrader/Config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));  // your React app origin
+        config.setAllowedOrigins(List.of("https://student-grader-frontend-flame.vercel.app/"));  // your React app origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowCredentials(true);
